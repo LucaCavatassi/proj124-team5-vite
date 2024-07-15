ccd038374bf98d002fbd264dd22f60b7681bbdf6
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-07-15 17:18:31 +0200

    feat: installed vue typewriter effect

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,21 +1,39 @@
 <script>
+        import {VueWriter} from 'vue-writer'
     export default {
-        name: "HomePage"
+        name: "HomePage",
+        components: {VueWriter}
     }
+
+
 </script>
 
 <template>
     <div class="container">
-        <h1>Homepage</h1>   
-
+        <h1> Voglio andare a <VueWriter class="d-inline" :eraseSpeed="50" :array="[
+    'Londra',
+    'Parigi',
+    'Roma',
+    'Berlino',
+    'Madrid',
+    'Vienna',
+    'Praga',
+    'Amsterdam',
+    'Lisbona',
+    'Atene',
+    'Milano',
+]" /></h1> 
     </div>
 </template>
 
-<style>
+<style scoped lang="scss">
     @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap');
 
     .container {
         font-family: "Quicksand", Arial, Helvetica, sans-serif;
+        h1{
+            font-weight: bold;
+        }
     }
 
 </style>
