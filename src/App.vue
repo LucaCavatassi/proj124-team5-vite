8ba95eb875dca9e061233739e8c6e337a3e88c34
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-07-15 12:23:43 +0200

    feat:navbar basic

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 
 <script>
 import axios from "axios"
+import AppNavBar from "./components/AppNavbar.vue"
+export default {
+  components: {
+    AppNavBar
+  },
+}
 
 </script>
 
 <template>
+  <AppNavBar/>
   <router-view></router-view>
 </template>
 
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<template>
+    <h1>navbar</h1>
+</template>
+
+<style>
+</style>
