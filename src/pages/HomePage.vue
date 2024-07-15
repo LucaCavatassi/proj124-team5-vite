c4b4c5b7cb61632231b155360b374c04c32b6018
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-07-15 15:53:06 +0200

    feat: installe typewriter effect

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,8 +5,17 @@
 </script>
 
 <template>
-    <h1>Homepage</h1>
+    <div class="container">
+        <h1>Homepage</h1>   
+
+    </div>
 </template>
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap');
+
+    .container {
+        font-family: "Quicksand", Arial, Helvetica, sans-serif;
+    }
+
 </style>
