9fef2e08e6bbcaaf7983c41560c68e50df0f66bb
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-07-15 12:34:53 +0200

    feat: logo navbar

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -2,8 +2,19 @@
 </script>
 
 <template>
-    <h1>navbar</h1>
+    <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+        <router-link :to="{ name: 'home' }" class="navbar-brand">
+            <img src="/src/assets/boolbnb_logo.jpeg" alt="Logo" class="d-inline-block align-text-top">
+        </router-link>
+    </div>
+</nav>
 </template>
 
-<style>
+<style scoped lang="scss">
+    .container-fluid {
+        img {
+            width: 15%;
+        }
+    }
 </style>
