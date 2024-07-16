ce5f65bbfb7efbfd29703081d3e6310c063f6440
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-07-16 10:36:24 +0200

    feat: responsive carousel and centered search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -2,12 +2,24 @@
 </script>
 
 <template>
-    <form class="form-inline my-2 my-lg-0">
+    <form class="form-inline my-2 my-lg-0 d-flex">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
 </template>
 
-<style>
+<style scoped lang="scss">
+    @use "../style/partials/palette" as *;
 
+
+  .btn-outline-success {
+    --bs-btn-color: #FE5D26;
+    --bs-btn-border-color:#FE5D26;
+    --bs-btn-hover-bg: #FE5D26;
+    --bs-btn-hover-border-color: #FE5D26;
+    --bs-btn-active-bg: #FE5D26;
+    --bs-btn-active-border-color: #FE5D26;
+    --bs-btn-disabled-color: #FE5D26;
+    --bs-btn-disabled-border-color: #FE5D26;
+  }
 </style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -17,9 +17,9 @@
 <template>
     <div class="ms_cont">
         <CarouselJumbo/>
-        <div class="container">
+        <div class="container d-flex flex-column justify-content-center py-2">
             <div class="row">
-                <h1> Voglio andare a <VueWriter class="d-inline" :eraseSpeed="50" :array="['...','Londra','Parigi','Roma','Berlino','Madrid','Vienna','Praga','Amsterdam','Lisbona','Atene','Milano']"/></h1> 
+                <h1> Voglio andare a <VueWriter style="color: #FE5D26;" class="d-inline" :eraseSpeed="50" :array="['Londra','Parigi','Roma','Berlino','Madrid','Milano']"/></h1> 
             </div>
             
             <div class="row">
@@ -35,6 +35,7 @@
     @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap');
 
     .container {
+        height: 100vh;
         font-family: "Quicksand", Arial, Helvetica, sans-serif;
         padding-top: 100px;
         position: relative;
