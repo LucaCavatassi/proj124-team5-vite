d5533a720c99866bd268c381a65921646163237d
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-07-17 18:48:21 +0200

    feat: problem stil running

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,6 +1,7 @@
 <script>
   import axios from 'axios';
   import { store } from "/src/store.js";
+  
 
   export default {
     data () {
@@ -11,18 +12,25 @@
 
     methods: {
       sendString() {
-        axios.post("http://127.0.0.1:8000/api/apartments/search/send-location", {
-          myLocation: this.store.location
-        }).then((resp)=> {console.log(resp);});
+        try {
+          window.csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+          axios.post("http://127.0.0.1:8000/api/apartments/send-location", {
+            _token: csrfToken,
+            myLocation : this.store.location,
+          },
+        ).then((resp)=> {console.log(resp);});
+        } catch (error) {
+          console.log(error);
+        }
       }
     }
   }
 </script>
 
 <template>
-    <form class="form-inline my-2 my-lg-0 d-flex">
+    <form method="POST" class="form-inline my-2 my-lg-0 d-flex">
       <input v-model="this.store.location" id="location" class="form-control mr-sm-2" type="search" placeholder="Inserisci la tua destinazione" aria-label="Search" required>
-      <router-link :to="{ name: 'results' }" id="search-btn" class="btn btn-outline-success my-2 my-sm-0" @click="sendString" type="submit">Cerca</router-link>
+      <router-link :to="{ name: 'results' }" ><button id="search-btn" class="btn btn-outline-success my-2 my-sm-0" @click="sendString" type="submit">Cerca</button></router-link>
     </form>
 </template>
 
