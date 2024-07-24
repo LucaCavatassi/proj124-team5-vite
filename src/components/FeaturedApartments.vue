f1187782e8787807d14499732effb17f029b7702
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-07-24 09:15:37 +0200

    featured apartment btn

--- src/components/FeaturedApartments.vue
+++ src/components/FeaturedApartments.vue
@@ -1,89 +1,89 @@
 <script>
-import axios from "axios";
-import { debounce } from "lodash";
-import { store } from "../store";
+	import axios from "axios";
+	import { debounce } from "lodash";
+	import { store } from "../store";
 
-export default {
-	data() {
-		return {
-			paginatedFeatured: [],
-			allFeatured: [],
-			curPage: 1,
-			totalPage: 0,
-			screenSize: "large",
-			store,
-			slug: null,
-		};
-	},
-	created() {
-		this.debouncedUpdateScreenSize = debounce(this.updateScreenSize, 300);
-		// Initial check to set the screen size
-		this.updateScreenSize();
+	export default {
+		data() {
+			return {
+				paginatedFeatured: [],
+				allFeatured: [],
+				curPage: 1,
+				totalPage: 0,
+				screenSize: "large",
+				store,
+				slug: null,
+			};
+		},
+		created() {
+			this.debouncedUpdateScreenSize = debounce(this.updateScreenSize, 300);
+			// Initial check to set the screen size
+			this.updateScreenSize();
 
-		// Add event listener for window resize
-		window.addEventListener("resize", this.debouncedUpdateScreenSize);
-		console.log(this.store);
-	},
-
-	methods: {
-		updateScreenSize() {
-			// Check if the window width matches the media query
-			if (window.matchMedia("(min-width: 600px)").matches) {
-				this.screenSize = "large";
-				this.fetchFeatured();
-			} else {
-				this.screenSize = "small";
-				this.fetchFeaturedAll();
-			}
+			// Add event listener for window resize
+			window.addEventListener("resize", this.debouncedUpdateScreenSize);
+			console.log(this.store);
 		},
 
-		beforeDestroy() {
-			// Remove the event listener when the component is destroyed
-			window.removeEventListener("resize", this.debouncedUpdateScreenSize);
-		},
+		methods: {
+			updateScreenSize() {
+				// Check if the window width matches the media query
+				if (window.matchMedia("(min-width: 600px)").matches) {
+					this.screenSize = "large";
+					this.fetchFeatured();
+				} else {
+					this.screenSize = "small";
+					this.fetchFeaturedAll();
+				}
+			},
 
-		fetchFeatured() {
-			axios
-				.get("http://127.0.0.1:8000/api/featured", {
-					params: {
-						page: this.curPage,
-					},
-				})
-				.then((resp) => {
-					// console.log(resp);
-					this.totalPage = resp.data.last_page;
-					this.paginatedFeatured = resp.data["data"];
+			beforeDestroy() {
+				// Remove the event listener when the component is destroyed
+				window.removeEventListener("resize", this.debouncedUpdateScreenSize);
+			},
+
+			fetchFeatured() {
+				axios
+					.get("http://127.0.0.1:8000/api/featured", {
+						params: {
+							page: this.curPage,
+						},
+					})
+					.then((resp) => {
+						// console.log(resp);
+						this.totalPage = resp.data.last_page;
+						this.paginatedFeatured = resp.data["data"];
+					});
+			},
+
+			fetchFeaturedAll() {
+				axios.get("http://127.0.0.1:8000/api/featured-mobile").then((resp) => {
+					this.allFeatured = resp.data;
+					// console.log(resp.data);
 				});
-		},
+			},
 
-		fetchFeaturedAll() {
-			axios.get("http://127.0.0.1:8000/api/featured-mobile").then((resp) => {
-				this.allFeatured = resp.data;
-				// console.log(resp.data);
-			});
-		},
+			nextPage() {
+				if (this.curPage < this.totalPage) {
+					this.curPage++;
+					this.fetchFeatured();
+				} else {
+					this.curPage = 1;
+					this.fetchFeatured();
+				}
+			},
 
-		nextPage() {
-			if (this.curPage < this.totalPage) {
-				this.curPage++;
-				this.fetchFeatured();
-			} else {
-				this.curPage = 1;
-				this.fetchFeatured();
-			}
+			prevPage() {
+				if (this.curPage > 1) {
+					this.curPage--;
+					this.fetchFeatured();
+				} else {
+					this.curPage = this.totalPage;
+					this.fetchFeatured();
+				}
+			},
 		},
-
-		prevPage() {
-			if (this.curPage > 1) {
-				this.curPage--;
-				this.fetchFeatured();
-			} else {
-				this.curPage = this.totalPage;
-				this.fetchFeatured();
-			}
-		},
-	},
-};
+	};
 </script>
 
 <template class="ms_wrap">
@@ -107,17 +107,14 @@ export default {
 		<div class="row">
 			<div class="col-4 py-5" v-for="featured in this.paginatedFeatured">
 				<div class="card">
-					<img
-						src="https://www.apartments.com/blog/sites/default/files/styles/x_large_hq/public/image/2023-06/ParkLine-apartment-in-Miami-FL.jpg?itok=kQmw64UU"
-						class="card-img-top"
-						alt=""
-					/>
+					<img src="https://www.apartments.com/blog/sites/default/files/styles/x_large_hq/public/image/2023-06/ParkLine-apartment-in-Miami-FL.jpg?itok=kQmw64UU"
+						class="card-img-top" alt="" />
 					<div class="card-body">
 						<h5 class="card-title">{{ featured.title }}</h5>
 						<p class="card-text">{{ featured.apartment_description }}</p>
-					
+
 						<router-link :to="{ name: 'single-result', params: { slug: featured.slug } }"
-                            class="btn btn-primary ms_btn">Scopri di più</router-link>
+							class="btn btn-primary ms_btn">Scopri di più</router-link>
 					</div>
 				</div>
 			</div>
@@ -128,17 +125,14 @@ export default {
 		<div class="row">
 			<div class="col-12 py-5" v-for="featured in this.allFeatured">
 				<div class="card">
-					<img
-						src="https://www.apartments.com/blog/sites/default/files/styles/x_large_hq/public/image/2023-06/ParkLine-apartment-in-Miami-FL.jpg?itok=kQmw64UU"
-						class="card-img-top"
-						alt=""
-					/>
+					<img src="https://www.apartments.com/blog/sites/default/files/styles/x_large_hq/public/image/2023-06/ParkLine-apartment-in-Miami-FL.jpg?itok=kQmw64UU"
+						class="card-img-top" alt="" />
 					<div class="card-body">
 						<h5 class="card-title">{{ featured.title }}</h5>
 						<p class="card-text">{{ featured.apartment_description }}</p>
 						<a href="#" class="btn ms_btn">Scopri di più</a>
-						<!-- <router-link :to="{ name: 'single-result', params: { slug: store.searchResults.slug } }"
-                            class="btn btn-primary ms_btn">Scopri di più</router-link> -->
+						<router-link :to="{ name: 'single-result', params: { slug: featured.slug } }"
+							class="btn btn-primary ms_btn">Scopri di più</router-link>
 					</div>
 				</div>
 			</div>
@@ -147,26 +141,26 @@ export default {
 </template>
 
 <style scoped lang="scss">
-@use "../style/partials/palette" as *;
+	@use "../style/partials/palette" as *;
 
-.card {
-	border-color: $green;
-	height: 100%;
-}
-
-.ms_btn {
-	background-color: $orange;
-	color: $white;
-}
-
-.ms_btn:hover {
-	background-color: #fb7a4f;
-	color: $white;
-}
-
-@media only screen and (min-width: 600px) {
-	.ms_wrap {
-		background-color: aqua;
+	.card {
+		border-color: $green;
+		height: 100%;
 	}
-}
+
+	.ms_btn {
+		background-color: $orange;
+		color: $white;
+	}
+
+	.ms_btn:hover {
+		background-color: #fb7a4f;
+		color: $white;
+	}
+
+	@media only screen and (min-width: 600px) {
+		.ms_wrap {
+			background-color: aqua;
+		}
+	}
 </style>
